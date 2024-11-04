e2924fc192ba7b383be422f35e2b62eee10c5d55
Shoki Miyagawa <smygw72@gmail.com>
2024-11-04 15:53:50 +0900

    fix

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -429,18 +429,18 @@ func getLogout(w http.ResponseWriter, r *http.Request) {
 func getIndex(w http.ResponseWriter, r *http.Request) {
 	me := getSessionUser(r)
 
-	results := []Post{}
+	posts := []Post{}
 
 	// Post tableと User table をjoinしてdelflagが0のものだけを20件取得する
 	query := "SELECT p.id, p.user_id, p.body, p.mime, p.created_at, u.id, u.account_name, u.passhash, u.authority, u.del_flg, u.created_at " +
 		"FROM `posts` as p JOIN `users` as u ON p.user_id = u.id " +
 		"WHERE u.del_flg = 0 ORDER BY p.created_at DESC LIMIT ?"
-	err := db.Select(&results, query, postsPerPage)
+	err := db.Select(&posts, query, postsPerPage)
 	if err != nil {
 		log.Print(err)
 		return
 	}
-	for _, p := range results {
+	for i, p := range posts {
 		err := db.Get(&p.CommentCount, "SELECT COUNT(*) AS `count` FROM `comments` WHERE `post_id` = ?", p.ID)
 		if err != nil {
 			log.Print(err)
@@ -468,9 +468,9 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 			comments[i], comments[j] = comments[j], comments[i]
 		}
 
-		p.Comments = comments
+		posts[i].Comments = comments
 
-		p.CSRFToken = getCSRFToken(r)
+		posts[i].CSRFToken = getCSRFToken(r)
 	}
 
 	// err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC")
